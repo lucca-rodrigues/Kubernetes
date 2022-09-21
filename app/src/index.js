d5638dfa72bf6b3d445ffbd8eca066627dfcfeb8
Lucas <lucas@MacBook-Air-de-Lucas.local>
2022-09-21 18:08:46 -0300

    feat: implement configmap

--- app/src/index.js
+++ app/src/index.js
@@ -1,11 +1,13 @@
 const express = require("express");
+require("dotenv").config();
 
 var app = express();
+const port = process.env.PORT ?? 3333;
 
 app.get("/", function (req, res) {
-  res.send("Hello World");
+  res.send(`Hello World, app ${process.env.APINAME ?? "kubernentes"} started on port ${port}`);
 });
 
-app.listen(3333, () => {
-  console.log("app started on port 3333");
+app.listen(port, () => {
+  console.log(`app ${process.env.APINAME ?? "kubernentes"} started on port ${port}`);
 });
